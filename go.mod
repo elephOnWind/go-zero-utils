7abf26d832c0ac6334198d0fcbd0c95a3f555e7e
GEMDALE\chenwei14 <chenwei14@gemdale.com>
2021-08-19 11:26:36 +0800

    add response

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/elephonwind/go-zero-utils
+
+go 1.16
+
+require github.com/tal-tech/go-zero v1.1.10
---
+++ response/response.go
@@ -0,0 +1,46 @@
+package reponse
+
+import (
+	"net/http"
+
+	"github.com/tal-tech/go-zero/rest/httpx"
+)
+
+const (
+	DEFAULT_SUCCESS_CODE      = 0
+	DEFAULT_ERROR_CODE        = 500
+	DEFAULT_PARAMS_ERROR_CODE = 300
+)
+
+type Body struct {
+	Code      int         `json:"code"`
+	Msg       string      `json:"msg"`
+	Data      interface{} `json:"data,omitempty"`
+	Timestamp int64       `json:"timestamp,string"`
+}
+
+func Success(w http.ResponseWriter, resp interface{}) {
+	var body Body
+	body.Code = DEFAULT_SUCCESS_CODE
+	body.Msg = "Success"
+	body.Data = resp
+	httpx.OkJson(w, body)
+}
+func Error(w http.ResponseWriter, err error) {
+	var body Body
+	body.Code = DEFAULT_ERROR_CODE
+	body.Msg = err.Error()
+	body.Data = nil
+	httpx.OkJson(w, body)
+}
+func Response(w http.ResponseWriter, code int, resp interface{}, err error) {
+	var body Body
+	body.Code = code
+	if err != nil {
+		body.Msg = err.Error()
+	} else {
+		body.Msg = "OK"
+		body.Data = resp
+	}
+	httpx.OkJson(w, body)
+}
